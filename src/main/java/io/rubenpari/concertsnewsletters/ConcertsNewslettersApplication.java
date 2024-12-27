76597044ae29f338179b16fb7042cac93b7cbec9
RubenPari <rubenpari.developer@gmail.com>
2024-12-27 19:19:46 +0100

    initial commit

---
+++ src/main/java/io/rubenpari/concertsnewsletters/ConcertsNewslettersApplication.java
@@ -0,0 +1,13 @@
+package io.rubenpari.concertsnewsletters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertsNewslettersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertsNewslettersApplication.class, args);
+    }
+
+}

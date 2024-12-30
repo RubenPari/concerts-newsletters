587187079701cb9fc935783caae2ad84f5c827b9
RubenPari <rubenpari.developer@gmail.com>
2024-12-30 15:50:07 +0100

    added loadEnv with external library

--- src/main/java/io/rubenpari/concertsnewsletters/ConcertsNewslettersApplication.java
+++ src/main/java/io/rubenpari/concertsnewsletters/ConcertsNewslettersApplication.java
@@ -1,13 +1,27 @@
 package io.rubenpari.concertsnewsletters;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConcertsNewslettersApplication {
-
     public static void main(String[] args) {
+        try {
+            String currentDirectory = System.getProperty("user.dir");
+            String envPath = currentDirectory + "/.env";
+
+            Dotenv dotenv = Dotenv.configure()
+                    .directory(envPath)
+                    .load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+            System.out.println(System.getenv("GEMINI_API_KEY"));
+        } catch (Exception e) {
+            System.out.println("Error occurred while loading environment variables:");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
         SpringApplication.run(ConcertsNewslettersApplication.class, args);
     }
-
 }

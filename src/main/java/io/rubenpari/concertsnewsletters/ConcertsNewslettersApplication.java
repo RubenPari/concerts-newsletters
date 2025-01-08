c36fc8f7811b69f0d8490074259186e94c1a966d
RubenPari <rubenpari.developer@gmail.com>
2025-01-08 16:58:40 +0100

    removed Environment class

--- src/main/java/io/rubenpari/concertsnewsletters/ConcertsNewslettersApplication.java
+++ src/main/java/io/rubenpari/concertsnewsletters/ConcertsNewslettersApplication.java
@@ -1,14 +1,11 @@
 package io.rubenpari.concertsnewsletters;
 
-import io.rubenpari.concertsnewsletters.utils.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConcertsNewslettersApplication {
     public static void main(String[] args) {
-        Environment.loadEnv();
-
         SpringApplication.run(ConcertsNewslettersApplication.class, args);
     }
 }
